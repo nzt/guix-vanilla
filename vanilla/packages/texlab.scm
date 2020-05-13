81e01a49c5f7059ad69fc410a8facbfd8a35670e
TANIGUCHI Masaya <ta2gch@gmail.com>
2020-05-13 12:52:03 +0000

    Export packages

--- vanilla/packages/texlab.scm
+++ vanilla/packages/texlab.scm
@@ -4,23 +4,24 @@
   #:use-module (guix build-system copy)
   #:use-module (guix licenses))
 
-(package
- (name "texlab")
- (version "2.1.0")
- (source (origin
-          (method url-fetch)
-          (uri (string-append "https://github.com/latex-lsp/texlab/releases/download/v" version "/texlab-x86_64-linux.tar.gz"))
-          (sha256
-           (base32 "1hpx1li2r4vfyj0v3gamlj2gmj8d6f5l71sdcqy0y1mq9zykmap0"))))
- (build-system copy-build-system)
- (arguments
-  `(#:install-plan '(("texlab" "bin/texlab"))
-    #:phases
-    (modify-phases
-     %standard-phases
-     (delete 'validate-runpath)
-     (replace 'unpack (lambda _ (invoke "tar" "zxf" (assoc-ref %build-inputs "source")))))))
- (synopsis "An implementation of the Language Server Protocol for LaTeX")
- (description "A cross-platform implementation of the Language Server Protocol for LaTeX.")
- (home-page "https://texlab.netlify.app/")
- (license gpl3+))
+(define-public texlab
+    (package
+    (name "texlab")
+    (version "2.1.0")
+    (source (origin
+            (method url-fetch)
+            (uri (string-append "https://github.com/latex-lsp/texlab/releases/download/v" version "/texlab-x86_64-linux.tar.gz"))
+            (sha256
+            (base32 "1hpx1li2r4vfyj0v3gamlj2gmj8d6f5l71sdcqy0y1mq9zykmap0"))))
+    (build-system copy-build-system)
+    (arguments
+    `(#:install-plan '(("texlab" "bin/texlab"))
+        #:phases
+        (modify-phases
+        %standard-phases
+        (delete 'validate-runpath)
+        (replace 'unpack (lambda _ (invoke "tar" "zxf" (assoc-ref %build-inputs "source")))))))
+    (synopsis "An implementation of the Language Server Protocol for LaTeX")
+    (description "A cross-platform implementation of the Language Server Protocol for LaTeX.")
+    (home-page "https://texlab.netlify.app/")
+    (license gpl3+)))
