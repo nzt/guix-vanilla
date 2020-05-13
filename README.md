336ebd4ea191f741fde8c03cdba9fd90c12ee8a3
TANIGUCHI Masaya <ta2gch@gmail.com>
2020-05-13 11:41:10 +0000

    Add gopass and texlab

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # 🍦 GUIX Vanilla
 Original prebuilt package collection for GNU GUIX
+
+# Installation
+GUIX Vanilla can be installed as a Guix channel. To do so, add it to ~/.config/guix/channels.scm:
+
+```scheme
+(cons* (channel
+        (name 'vanilla)
+        (url "https://github.com/nzt/guix-vanilla"))
+       %default-channels)
+```
---
+++ vanilla/packages/gopass.scm
@@ -0,0 +1,29 @@
+(define-modules (vanilla packages gopass)
+   #:use-module (guix packages)
+   #:use-module (guix download)
+   #:use-module (guix build-system copy)
+   #:use-module (guix licenses))
+
+(package
+ (name "gopass")
+ (version "1.9.1")
+ (source (origin
+          (method url-fetch)
+          (uri (string-append "https://github.com/gopasspw/gopass/releases/download/v" version "/gopass-" version "-linux-amd64.tar.gz"))
+          (sha256
+           (base32 "1ysr90ay2jriq2nf505mvs3shqgw7jvc916s5hdpn9j3qzfzv3aa"))))
+ (build-system copy-build-system)
+ (arguments
+  `(#:install-plan '(("gopass" "bin/gopass")
+                     ("zsh.completion" "share/zsh/site-functions/_gopass")
+                     ("fish.completion" "share/fish/vendo_completions.d/gopass.fish")
+                     ("bash.completion" "etc/bash_completion.d/gopass"))
+    #:phases
+    (modify-phases
+     %standard-phases
+     (delete 'validate-runpath)
+     (replace 'unpack (lambda _ (invoke "tar" "zxf" (assoc-ref %build-inputs "source")))))))
+ (synopsis "The slightly more awesome standard unix password manager for teams")
+ (description "gopass is a rewrite of the pass password manager in Go with the aim of making it cross-platform and adding additional features. Our target audience are professional developers and sysadmins (and especially teams of those) who are well versed with a command line interface. One explicit goal for this project is to make it more approachable to non-technical users. We go by the UNIX philosophy and try to do one thing and do it well, providing a stellar user experience and a sane, simple interface.")
+ (home-page "https://texlab.netlify.app/")
+ (license mit))
